40c51fc1eac779bde87008cdc60330295aea5d30
Phyunsang <powerx1674@naver.com>
2024-01-29 17:49:12 +0900

    03_javascript

---
+++ 3_javascript/js/UI구현_이미지.js
@@ -0,0 +1,51 @@
+const click = document.querySelector("click");
+
+document.querySelector("click");
+
+
+
+function click() {
+    console.log("container")
+};
+
+const input =document.querySelector("#input");
+
+
+
+
+const fontColor = document.querySelector("fontColor");
+
+
+function fontColor() {
+    let = "red", let !== red
+    console.log(style.fontColor.red); 
+
+
+};
+
+
+const backgroundColor = document.querySelector("backgroundColor");
+
+function backgroundColor() {
+    let = "yellow",let !== yellow
+    console.log(style.backgroundColor.yellow)
+    
+    
+};
+
+
+function input(fontColor,backgroundColor) {
+    console.log("")
+    if (input !== red, yellow ) {
+        alert("색상을 다시 입력하시오")
+    }
+
+
+};
+
+input.addEventListener("input", () => {
+    document.querySelectorAll("click")
+
+});
+
+
